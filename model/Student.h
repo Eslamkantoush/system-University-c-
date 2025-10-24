9622f104703ea2ebff0bb60f5179ae7d7cec264e
Eslamkantoush <eslamkantoush0@gmail.com>
2025-10-24 22:48:51 +0300

    Relation Between course and student

--- model/Student.h
+++ model/Student.h
@@ -1,11 +1,13 @@
 #pragma once
 #include"BaseEntity.h"
 #include "professor.h"
+#include"course.h"
 class Student : public BaseEntity
 {
 private: 
 	float Gpa;
 	professor prof_arr[5];
+	course cour_arr[5];
 public:
 	// setter //
 	void set_Gpa(float Gpa) {
@@ -13,10 +15,16 @@ public:
 	}
 
 	void set_professor(professor prof_arr[5]) {
-		for (int i = 0; i < sizeof(prof_arr)/ sizeof(prof_arr[0]); i++)
+		
+		for (int i = 0; i < sizeof(prof_arr) / sizeof(prof_arr[0]); i++)
 			this->prof_arr[i] = prof_arr[i];
 	}
 
+
+	void set_course(course cour_arr[5]) {
+		for (int i = 0; i < sizeof(cour_arr) / sizeof(cour_arr[0]); i++)
+			this->cour_arr[i] = cour_arr[i];
+	}
 	//getter//
 
 	float get_Gpa() const {
@@ -25,6 +33,10 @@ public:
 
 	const professor * get_professor() const {
 		return prof_arr;
+	}
+
+	const course* set_course() const {
+		return cour_arr;
 	}
 };
 
--- model/course.h
+++ model/course.h
@@ -2,18 +2,28 @@
 #include"sharData.h"
 class course :sharData
 {
-private:
+protected:
 	float hour;
+	int studentIds[5];
 public:
 	// setter //
 	void set_hour(float hour) {
 		this->hour = hour;
 	}
 
+	void set_studentIds(int studentIds[5]) {
+		for (int i = 0; i < sizeof(studentIds) / sizeof(studentIds[0]); i++)
+			this->studentIds[i] = studentIds[i];
+	}
 
+	
 	// Getter //
 	float get_hour() const {
 		return hour;
+	}
+
+	const int* get_studentIds() {
+		return studentIds;
 	}
 };
 
