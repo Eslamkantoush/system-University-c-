0f844d1b30545d48f292b60d6bed8de223ab2ef1
Eslamkantoush <eslamkantoush0@gmail.com>
2025-10-24 21:04:56 +0300

    create model BaseEntity

--- model/professor.h
+++ model/professor.h
@@ -1,55 +1,18 @@
 #pragma once
-#include<string>
-class professor
+#include"BaseEntity.h"
+class professor : public BaseEntity
 {
 private:
-	int id;
-	std::string age;
-	std::string name;
 	float Salary;
-	std::string Phon_num;
 public:
 	// setter //
-	void set_id(int id) {
-		this->id = id;
-	}
-
-	void set_age(std::string age) {
-		this->age = age;
-	}
-
-	void set_name(std::string name) {
-		this->name = name;
-	}
-
 	void set_Salary(float Salary) {
 		this->Salary = Salary;
 	}
 
-	void set_Phon_num(std::string Phon_num) {
-		this->Phon_num = Phon_num;
-	}
-
-	//getter//
-
-	int get_id() {
-		return id;
-	}
-
-	std::string get_age() {
-		return age;
-	}
-
-	std::string get_name() {
-		return name;
-	}
-
+	// Getter //
 	float get_Salary() {
 		return Salary;
-	}
-
-	std::string get_Phon_num() {
-		return Phon_num;
 	}
 };
 
